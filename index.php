3a330000789e5dc99f5b671e2b4b6872e38f91d9
adem-louriachi <louriachi.adem@gmail.com>
2019-10-02 17:06:33 +0200

    Squelette de l'HTML

--- index.php
+++ index.php
@@ -1,3 +1,52 @@
 <!doctype html>
 
-<?php
+<html lang="fr">
+
+    <head>
+    <!-- Balise Meta -->
+        <meta charset="UTF-8"/>
+        <meta name="author" content="Paul Chabas, Vincent Ferrer, Adem Louriachi, Guillaume Piccina" />
+        <meta name="description" content="Site FreeNote Projet Web - 3eme semestre - DUT Informatique, professeur : Olivier Gérard"/>
+    <!-- Stylesheet -->
+        <link rel="stylesheet" href="index.css"/>
+
+        <title>FreeNote by 4randoms</title>
+
+    </head>
+
+    <body>
+
+        <div id="index"> <!-- debut du div de la page -->
+
+            <header> <!-- en-tête -->
+
+                <div id="header">
+
+
+
+                </div>
+
+            </header>
+
+            <div id="main">
+
+
+
+            </div>
+
+            <footer>
+
+                <div id="footer">
+
+
+
+                </div>
+
+            </footer>
+
+        </div>
+
+    </body>
+
+
+</html>
