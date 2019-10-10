3ae0fc8bad3bd82c623d7f2b41e011d59d998843
Vincent Ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-10-10 17:21:40 +0200

    ConnectBD en cours

--- Models/LinkBD.php
+++ Models/LinkBD.php
@@ -1,1 +1,15 @@
 <?php
+try{
+    // Connexion à la base de donnee
+    $dsn = 'mysql:host localhost;dbname my_dbname';
+    $pdo = new PDO($dsn , 'mysql_username', 'mysql_password');
+    // Codage de caractères
+    $pdo->exec ('SET CHARACTER SET utf8');
+    // Gestion des erreurs sous forme d'exceptions
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e) {
+    // Affichage de l'erreur
+    die ('Erreur ');
+}
+
