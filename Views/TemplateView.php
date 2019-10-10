244ae721429c822542f784a189731ab80b0d6aaa
paul-chabas15 <37959800+paul-chabas15@users.noreply.github.com>
2019-10-10 16:31:01 +0200

    Un petit peu de responsive
    C'est toujours sympa

--- Views/TemplateView.php
+++ Views/TemplateView.php
@@ -5,6 +5,7 @@
 <head>
     <!-- Balise Meta -->
     <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Paul Chabas, Vincent Ferrer, Adem Louriachi, Guillaume Piccina" />
     <meta name="description" content="Site FreeNote Projet Web - 3eme semestre - DUT Informatique, professeur : Olivier Gérard"/>
     <!-- Stylesheet -->
