35012aa701aaf65631f5c236caf0042fb1a75909
Adem <louriachi.adem@gmail.com>
2019-10-10 13:54:05 +0200

    Texte de présentation

--- Controlleurs/Utilisateur.php
+++ Controlleurs/Utilisateur.php
@@ -1,5 +1,4 @@
 <?php
-require('../Modèles/UtilisateursMod.php');
 
 class Utilisateur
 {
--- Vues/AccueilVue.php
+++ Vues/AccueilVue.php
@@ -56,11 +56,13 @@
             </div>
             <section>
                 <h1>Description du site</h1>
-                <p>Bienvenue sur FreeNote ! <br> Un réseau social où les messages privés n'existent pas. <br>
-                   Vous pouvez lancer une discussion sur un sujet (La mort de Jacques Chirac, Pourquoi il ne faut pas boire de Munster, L'utilité d'un BDE, Greta Thunberg, etc.).
-                   Puis, vous, cher.ère.s membres, pouvez participer à chaque discussion. <br>
-                   Cependant, nous ne sommes pas ici pour créer un basique forum. Chacun de vous ne peut écrire que deux mots dans un message sur toute une discussion ET C'EST TOUT. <br>
-                   Choisissez avec soin...</p>
+                <p>
+                    Bienvenue sur FreeNote ! <br> Un réseau social où les messages privés n'existent pas. <br>
+                    Vous pouvez lancer une discussion sur un sujet (La mort de Jacques Chirac, Pourquoi il ne faut pas boire de Munster, L'utilité d'un BDE, Greta Thunberg, etc.).
+                    Puis, vous, cher.ère.s membres, pouvez participer à chaque discussion. <br>
+                    Cependant, nous ne sommes pas ici pour créer un basique forum. Chacun de vous ne peut écrire que deux mots dans un message sur toute une discussion ET C'EST TOUT. <br>
+                    Choisissez avec soin...
+                </p>
             </section>
         </main>
 
