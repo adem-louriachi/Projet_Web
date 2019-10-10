c39b23f9e5825e12a4434df820f07c1e70e45b1f
p18003955 <guillaume.piccina@etu.univ-amu.fr>
2019-10-10 11:07:36 +0200

    création classe utilisateur controlleur

--- Controlleurs/Utilisateur.php
+++ Controlleurs/Utilisateur.php
@@ -1,28 +1,59 @@
 <?php
-    class Utilisateur{
-        private $nom;
-        private $email;
-        private $su;  // bool superutilisateur
+class Membre
+{
+    private $mail;
+    private $mdp;
+    private $pseudo;
+    private $admin = false;
 
-        public function __construct($name){
-            $this->nom = $name;
-        }
-        public function estConnecte(){
-            if(isset($this->nom)){
-                return True;
-            }
-            return False;
-        }
-
-        public function estSU(){
-            return $this->su;
-        }
-
-        public function recupNom(){
-            return $this->nom;
-        }
-
-        public function recupEmail(){
-            return $this->email;
-        }
+    public function getMail()
+    {
+        return $this->mail;
     }
+
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+    }
+
+    public function getMdp()
+    {
+        return $this->mdp;
+    }
+
+    public function setMdp($mdp)
+    {
+        $this->mdp = $mdp;
+    }
+
+    public function getPseudo()
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo($pseudo)
+    {
+        $this->pseudo = $pseudo;
+    }
+
+    public function getAdmin()
+    {
+        return $this->admin;
+    }
+
+    public function setAdmin($admin)
+    {
+        $this->admin = $admin;
+    }
+
+    public function __construct($_mail,$_mdp, $_pseudo) {
+        $this->setMail($_mail);
+        $this->setMdp($_mdp);
+        $this->setPseudo($_pseudo);
+    }
+
+    public function __destruct()
+    {
+
+    }
+}
