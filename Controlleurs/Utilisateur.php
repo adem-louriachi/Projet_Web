b4a4156a003a3367680ac9786577c958ca13cbb0
p18003955 <guillaume.piccina@etu.univ-amu.fr>
2019-10-10 11:16:03 +0200

    modif classe utilisateur controlleur

--- Controlleurs/Utilisateur.php
+++ Controlleurs/Utilisateur.php
@@ -1,55 +1,57 @@
 <?php
+require('../Modèles/UtilisateursMod.php');
+
 class Membre
 {
     private $mail;
     private $mdp;
     private $pseudo;
-    private $admin = false;
+    private $admin;
 
     public function getMail()
     {
-        return $this->mail;
+        return $this->mail; //requete pour recup dans la BD
     }
 
     public function setMail($mail)
     {
-        $this->mail = $mail;
+        $this->mail = $mail; //requete pour update dans la BD
     }
 
     public function getMdp()
     {
-        return $this->mdp;
+        return $this->mdp; //requete pour recup dans la BD
     }
 
     public function setMdp($mdp)
     {
-        $this->mdp = $mdp;
+        $this->mdp = $mdp; //requete pour update dans la BD
     }
 
     public function getPseudo()
     {
-        return $this->pseudo;
+        return $this->pseudo; //requete pour recup dans la BD
     }
 
     public function setPseudo($pseudo)
     {
-        $this->pseudo = $pseudo;
+        $this->pseudo = $pseudo; //requete pour update dans la BD
     }
 
     public function getAdmin()
     {
-        return $this->admin;
+        return $this->admin; //requete pour recup dans la BD
     }
 
     public function setAdmin($admin)
     {
-        $this->admin = $admin;
+        $this->admin = $admin; //requete pour update dans la BD
     }
 
     public function __construct($_mail,$_mdp, $_pseudo) {
-        $this->setMail($_mail);
-        $this->setMdp($_mdp);
-        $this->setPseudo($_pseudo);
+        $this->setMail($_mail); //requete pour insérer dans la BD
+        $this->setMdp($_mdp); //requete pour insérer dans la BD
+        $this->setPseudo($_pseudo); //requete pour insérer dans la BD
     }
 
     public function __destruct()
@@ -57,3 +59,5 @@ class Membre
 
     }
 }
+
+?>
