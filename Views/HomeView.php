cbfca96c348ef6b4514c00c105aad63d6d0b72f5
Adem LOURIACHI <49907382+adem-louriachi@users.noreply.github.com>
2019-10-10 16:15:46 +0200

    Update HomeView.php
    pfiou

--- Views/HomeView.php
+++ Views/HomeView.php
@@ -24,6 +24,16 @@
     <a>Déjà inscrit ?</a> <!-- Javascript à écrire pour passer la visibilité d'inscription à connexion -->
     <a>Pas encore de compte ?</a>
 </div>
+<section>
+    <h2>Description du site</h2>
+        <p>
+        Bienvenue sur FreeNote ! <br> Un réseau social où les messages privés n'existent pas. <br>
+        Vous pouvez lancer une discussion sur un sujet (La mort de Jacques Chirac, Pourquoi il ne faut pas boire de Munster, L'utilité d'un BDE, Greta Thunberg, etc.).
+        Puis, vous, cher.ère.s membres, pouvez participer à chaque discussion. <br>
+        Cependant, nous ne sommes pas ici pour créer un basique forum. Chacun de vous ne peut écrire que deux mots dans un message sur toute une discussion ET C'EST TOUT. <br>
+        Choisissez avec soin...
+        </p>
+</section>
 <?php
     $content = ob_get_clean();
     require 'TemplateView.php';
