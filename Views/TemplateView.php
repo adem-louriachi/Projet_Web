edb98651e6b6342a07bf043fd3196334fd7bb4e6
paul-chabas15 <37959800+paul-chabas15@users.noreply.github.com>
2019-10-10 16:46:04 +0200

    Ajout du alt pour le logo

--- Views/TemplateView.php
+++ Views/TemplateView.php
@@ -28,7 +28,7 @@
 
 <header> <!-- en-tête -->
 
-    <img src="https://imagizer.imageshack.com/img923/196/79WmYc.png">
+    <img alt="Logo de FreeNote" src="https://imagizer.imageshack.com/img923/196/79WmYc.png">
 
 </header>
 
