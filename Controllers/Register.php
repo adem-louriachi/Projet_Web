81ef40f87a5e39813ad6b0ec17d41688b41d6921
paul-chabas15 <37959800+paul-chabas15@users.noreply.github.com>
2019-10-14 18:49:50 +0200

    Scénario de mot de passe différent à l'inscription
    Affichage de l'erreur et récupération des valeurs entrées

--- Controllers/Register.php
+++ Controllers/Register.php
@@ -1,4 +1,14 @@
 <?php
+    session_start();
+    $_SESSION['errors'] = array();
     $nick = $_POST['nick'];
     $email = $_POST['email'];
     $pwd = $_POST['pwd'];
+    $pwdconf = $_POST['pwdconf'];
+
+    if ($pwd != $pwdconf) {
+        $_SESSION['errors'][] = 'Les mots de passe ne sont pas les mêmes';
+        $_SESSION['email'] = $email;
+        $_SESSION['nick'] = $nick;
+        header('Location: ../index.php');
+    }
