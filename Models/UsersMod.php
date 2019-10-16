73d99871a725e245a3a903d5ec53e6710c08a1ed
p18003955 <guillaume.piccina@etu.univ-amu.fr>
2019-10-16 11:43:44 +0200

    requete creation d'utilisateur

--- Models/UsersMod.php
+++ Models/UsersMod.php
@@ -1,1 +1,8 @@
+<?php
+    require 'LinkBD.php';
+
+    function createUser($nick, $email, $pwd) {
+        $createUser = ConnectBD()->query("insert into Utilisateurs(nom, mail, MotDePasse, SuperUtilisateur) values ('$nick','$email','$pwd')");
+        return $createUser;
+    }
 
