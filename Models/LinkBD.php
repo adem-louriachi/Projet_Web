0e8b1d187c2b42986868581359a4150ed3c34293
Vincent Ferrer <45425884+vincent-ferrer@users.noreply.github.com>
2019-10-16 11:28:06 +0200

    Update LinkBD.php

--- Models/LinkBD.php
+++ Models/LinkBD.php
@@ -1,20 +1,4 @@
 <?php
-<<<<<<< HEAD
-try{
-    // Connexion à la base de donnee
-    $dsn = 'mysql:host localhost;dbname my_dbname';
-    $pdo = new PDO($dsn , 'mysql_username', 'mysql_password');
-    // Codage de caractères
-    $pdo->exec ('SET CHARACTER SET utf8');
-    // Gestion des erreurs sous forme d'exceptions
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e) {
-    // Affichage de l'erreur
-    die ('Erreur ');
-}
-
-=======
 
 function ConnectBD()
 {
