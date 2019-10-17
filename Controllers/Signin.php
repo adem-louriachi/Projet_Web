7bf1853e5243e8718cc656d58a4e91d44e4ae292
p18003955 <guillaume.piccina@etu.univ-amu.fr>
2019-10-17 08:56:48 +0200

    amélioration Signin et UserMods

--- Controllers/Signin.php
+++ Controllers/Signin.php
@@ -1,14 +1,20 @@
 <?php
+    require "../Models/UsersMod.php";
+
+
     if (isset($_POST['login'],$_POST['pwd']))
     {
-        if ($login == $_POST['login'] &&  $pwd == md5($_POST['pwd']))
+        $pwd = getPwdBD($_POST['login']);
+
+
+        if ($nick == $_POST['login'] &&  $pwd == md5($_POST['pwd']))
         {
             session_start();
             $_SESSION['login'] = $_POST['login'];
             $_SESSION['pwd'] = md5($_POST['login']);
             //afficher page utilisateur
         }
-        else if ($login == $_POST['login'] && $pwd != md5($_POST['pwd']))
+        else if ($nick == $_POST['login'] && $pwd != md5($_POST['pwd']))
         {
             //erreur de mdp
         }
