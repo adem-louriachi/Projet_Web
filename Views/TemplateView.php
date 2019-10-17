0f75731fe1242b831cd04582117ee733c11cb03a
Adem <louriachi.adem@gmail.com>
2019-10-17 15:12:36 +0200

    test navbar 13

--- Views/TemplateView.php
+++ Views/TemplateView.php
@@ -22,11 +22,12 @@
     <div class="navbar-fixed">
         <nav>
             <div class="nav-wrapper black">
-        <a href="../index.php"><img alt="Logo de FreeNote" src="https://image.noelshack.com/fichiers/2019/41/4/1570720588-free-2.png"></a>
-                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <a href="../index.php"><img class="brand-logo nav-wrapper" alt="Logo de FreeNote" src="https://image.noelshack.com/fichiers/2019/41/4/1570720588-free-2.png"></a>
+                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
             <?php include 'Controllers/AuthenticationCheck.php';?>
                 </ul>
+                </a>
         </div>
         </nav>
     </div>
