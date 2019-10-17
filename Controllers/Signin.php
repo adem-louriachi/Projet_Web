d719c4d7d12ae8edb47d08ceffcaea8f1e5361d8
p18003955 <guillaume.piccina@etu.univ-amu.fr>
2019-10-17 08:48:20 +0200

    page signin

--- Controllers/Signin.php
+++ Controllers/Signin.php
@@ -1,1 +1,19 @@
 <?php
+    if (isset($_POST['login'],$_POST['pwd']))
+    {
+        if ($login == $_POST['login'] &&  $pwd == md5($_POST['pwd']))
+        {
+            session_start();
+            $_SESSION['login'] = $_POST['login'];
+            $_SESSION['pwd'] = md5($_POST['login']);
+            //afficher page utilisateur
+        }
+        else if ($login == $_POST['login'] && $pwd != md5($_POST['pwd']))
+        {
+            //erreur de mdp
+        }
+        else
+        {
+            //erreur ce pseudo n'existe pas
+        }
+    }
