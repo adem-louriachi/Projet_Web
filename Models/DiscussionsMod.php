8b1fa1cf65e2e716d3a6dbcaf65b5836687792ac
p18003955 <guillaume.piccina@etu.univ-amu.fr>
2019-10-17 15:18:55 +0200

    changement requete UserMod et creation DiscussionMod

---
+++ Models/DiscussionsMod.php
@@ -0,0 +1,7 @@
+<?php
+    function createDiscussion($name,$author) {
+
+        $pdo = ConnectBD();
+        $pdo->query("INSERT INTO Discussion (NomDiscussion, Createur, EstOuvert) VALUES ($name, $author, '1')");
+    }
+
--- Models/UsersMod.php
+++ Models/UsersMod.php
@@ -4,24 +4,22 @@
     function createUser($nick, $email, $pwd) {
 
         $pdo = ConnectBD();
-        $sql = "INSERT INTO Utilisateurs(Nom, Mail, MotDePasse, SuperUtilisateur) VALUES ($nick,$email,$pwd, '0')";
-
+        $pdo->query("INSERT INTO Utilisateurs(Nom, Mail, MotDePasse, SuperUtilisateur) VALUES ($nick,$email,md5($pwd), '0')");
 
     }
 
     function getPwdBD($nick) {
 
         $pdo = ConnectBD();
-        $sql = "SELECT MotDePasse FROM Utilisateurs WHERE Nom = $nick";
-
+        $pwd = $pdo->query("SELECT MotDePasse FROM Utilisateurs WHERE Nom = $nick");
+        return $pwd;
 
     }
 
     function setPwdBd($nick, $newPwd) {
 
         $pdo = ConnectBD();
-        $sql = "UPDATE Utilisateurs SET MotDePasse = $newPwd WHERE Nom = $nick";
-
+        $pdo->query("UPDATE Utilisateurs SET MotDePasse = md5($newPwd) WHERE Nom = $nick");
 
     }
 
@@ -31,6 +29,8 @@
         setPwdBd($nick, $newPwd);
         return $newPwd;
     }
+
+
 
 
 
