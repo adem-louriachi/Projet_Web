608f348420b4187118623cd915e64400ab5ae2a8
paul-chabas15 <37959800+paul-chabas15@users.noreply.github.com>
2019-10-17 15:00:47 +0200

    Messages de Test affichés
    Youpi ça marche mais c'est pas beau

--- Views/HomeView.php
+++ Views/HomeView.php
@@ -50,12 +50,19 @@
 
 </section>
 <?php
-$message = array('user' => 'Toto', 'contenu' => 'wow');
-$messages = array($message);
-foreach ($messages as $message): ?>
-    <article class="message">
-        <h1 class="userMessage"><?= $message['user'] ?></h1>
-        <p><?= $message['contenu'] ?></p>
+$discussion1 = array('user' => 'Paul', 'contenu' => 'Wow');
+$discussion2 = array('user' => 'Adem', 'contenu' => 'Materialize vie');
+$discussion3 = array('user' => 'Guillaume', 'contenu' => 'Le PHP');
+$discussion4 = array('user' => 'Vincent', 'contenu' => 'La BD');
+$discussion5 = array('user' => 'Toto', 'contenu' => 'wow');
+$discussion6 = array('user' => 'Paul', 'contenu' => 'Autoload magie');
+$discussions = array($discussion1, $discussion2,$discussion3,$discussion4,$discussion5,$discussion6);
+foreach ($discussions as $discussion): ?>
+    <article class="discussion">
+        <a>
+            <h1 class="userdiscussion"><?= $discussion['user'] ?></h1>
+            <p><?= $discussion['contenu'] ?></p>
+        </a>
     </article>
     <hr />
 <?php endforeach; ?>
