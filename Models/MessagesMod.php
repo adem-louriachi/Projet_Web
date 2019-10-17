a5fd5fb8387555b13142721d67762b6e673d1b8a
Vincent Ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-10-17 09:54:39 +0200

    Correction class message et ahout InsertMsg()

--- Models/MessagesMod.php
+++ Models/MessagesMod.php
@@ -1,8 +1,26 @@
     <?php
     require 'LinkBD.php';
-    
-    $pdo=ConnectBD();
+    require 'Controllers/Messages.php';
 
-    $sql = 'INSERT INTO Message VALUES IdDiscussion, TextMessage, EstOuvert
-             WHERE ';
-    $stmt = $pdo->prepare($sql);
+
+    function InsertMsg($IdD, $TextM, $Ouvert)
+    {
+        $pdo = ConnectBD();
+
+        $sql = 'INSERT INTO Message (IdDiscussion, TextMessage, Date, EstOuvert)  VALUES (' . $IdD . ', ' . $TextM . ', ' . $Ouvert . ')';
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->bindValue('IdDiscussion',$IdD, PDO::PARAM_INT);
+        $stmt->bindValue('TextMessages',$TextM, PDO::PARAM_STR);
+        $stmt->bindValue('EstOuvert',$Ouvert, PDO::PARAM_BOOL);
+
+        try{
+            $stmt->execute();
+        }
+        catch (PDOException $e){
+            echo 'Erreur ', PHP_EOL;
+            echo 'Requete : ',$sql,PHP_EOL;
+            exit;
+        }
+
+    }
