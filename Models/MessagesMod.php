94861c81c01c48feaecebb58a5dd5232eda4169a
Vincent Ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-10-20 18:42:42 +0200

    Classe Abstraite Model et fonction InsertMsg

--- Models/MessagesMod.php
+++ Models/MessagesMod.php
@@ -1,26 +1,40 @@
     <?php
-    require 'LinkBD.php';
+    require 'Model.php.php';
     require 'Controllers/Messages.php';
 
+    class MessagesMod extends Model{
+        private $idMsg;    // autoincremente lors de l'ajout dans la BD
+        private $idDis;    // id discussion a qui appartient msg
+        private $dateMsg;  // current_timestamp dans BD
+        private $textMsg;
+        private $stateMsg; // bool
 
-    function InsertMsg($IdD, $TextM, $Ouvert)
-    {
-        $pdo = ConnectBD();
+        public function InsertMsg($idDis, $textMsg, $stateMsg){ // Inection SQL possible ???
+                $pdo = Model::connectBD();
+                $sql = 'INSERT INTO Message (IdDiscussion, TextMessage, Date, EstOuvert) VALUES (\''.$idDis.'\', \''.$textMsg.'\', \''.$stateMsg.'\')';
+                Model::executeQuery($pdo,$sql);
+        }
+
+
+    }
+
+   /* function InsertMsg($IdD, $TextM, $Ouvert){
+        $pdo = connectBD();
 
         $sql = 'INSERT INTO Message (IdDiscussion, TextMessage, Date, EstOuvert)  VALUES (' . $IdD . ', ' . $TextM . ', ' . $Ouvert . ')';
         $stmt = $pdo->prepare($sql);
 
-        $stmt->bindValue('IdDiscussion',$IdD, PDO::PARAM_INT);
-        $stmt->bindValue('TextMessages',$TextM, PDO::PARAM_STR);
-        $stmt->bindValue('EstOuvert',$Ouvert, PDO::PARAM_BOOL);
+        $stmt->bindValue('IdDiscussion', $IdD, PDO::PARAM_INT);
+        $stmt->bindValue('TextMessages', $TextM, PDO::PARAM_STR);
+        $stmt->bindValue('EstOuvert', $Ouvert, PDO::PARAM_BOOL);
 
-        try{
+        try {
             $stmt->execute();
-        }
-        catch (PDOException $e){
+        } catch (PDOException $e) {
             echo 'Erreur ', PHP_EOL;
-            echo 'Requete : ',$sql,PHP_EOL;
+            echo 'Requete : ', $sql, PHP_EOL;
             exit;
         }
+      }
 
-    }
+   */
