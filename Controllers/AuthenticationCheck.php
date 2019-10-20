a91612083b5c99bd858dda5d84e9b919d7081425
paul-chabas15 <37959800+paul-chabas15@users.noreply.github.com>
2019-10-20 15:35:19 +0200

    Empêche d'accéder au .git
    Jpp de Wamp

--- Controllers/AuthenticationCheck.php
+++ Controllers/AuthenticationCheck.php
@@ -1,12 +1,12 @@
 <?php
     ob_start();
     if (isset($_SESSION['user'])){
-        echo '<li><a href="/?ctrl=user&action=view">Mon compte</a></li><li><a href="/?ctrl=user&action=signout">Se déconnecter</a></li>';
+        echo '<li><a href="/?ctrl=User&action=view">Mon compte</a></li><li><a href="/?ctrl=User&action=signout">Se déconnecter</a></li>';
     }
     else{
         echo
-            '<li><a href="/?ctrl=user&action=signin">Se connecter</a></li>
-            <li><a href="/index.php?ctrl=user&action=register">S\'inscrire</a></li><li><a href="/index.php?ctrl=user&action=forget">Mot de passe oublié</a></li>';
+            '<li><a href="/?ctrl=User&action=signin">Se connecter</a></li>
+            <li><a href="/index.php?ctrl=User&action=register">S\'inscrire</a></li><li><a href="/index.php?ctrl=User&action=forget">Mot de passe oublié</a></li>';
     }
     /*
      *     if (isset($_SESSION['user'])){
