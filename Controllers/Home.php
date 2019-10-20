b02bc2849f8ccebd2011502b4f9eb8d634c3f7ca
paul-chabas15 <37959800+paul-chabas15@users.noreply.github.com>
2019-10-20 14:42:46 +0200

    Suppression des descriptions multiples, ajout d'un htaccess avec redirection vers HTTPS stricte et réécriture d'URL, sécurisation de l'accès au htaccess et au dossier .git
    C'est tout

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -12,45 +12,9 @@
         Cependant, nous ne sommes pas ici pour créer un basique forum. Chacun de vous ne peut écrire que deux mots dans un message sur toute une discussion ET C'EST TOUT. <br>
         Choisissez avec soin...
         </p>
-    <p>
-        Bienvenue sur FreeNote ! <br> Un réseau social où les messages privés n'existent pas. <br>
-        Vous pouvez lancer une discussion sur un sujet (La mort de Jacques Chirac, Pourquoi il ne faut pas boire de Munster, L'utilité d'un BDE, Greta Thunberg, etc.).
-        Puis, vous, cher.ère.s membres, pouvez participer à chaque discussion. <br>
-        Cependant, nous ne sommes pas ici pour créer un basique forum. Chacun de vous ne peut écrire que deux mots dans un message sur toute une discussion ET C'EST TOUT. <br>
-        Choisissez avec soin...
-    </p>
-    <p>
-        Bienvenue sur FreeNote ! <br> Un réseau social où les messages privés n'existent pas. <br>
-        Vous pouvez lancer une discussion sur un sujet (La mort de Jacques Chirac, Pourquoi il ne faut pas boire de Munster, L'utilité d'un BDE, Greta Thunberg, etc.).
-        Puis, vous, cher.ère.s membres, pouvez participer à chaque discussion. <br>
-        Cependant, nous ne sommes pas ici pour créer un basique forum. Chacun de vous ne peut écrire que deux mots dans un message sur toute une discussion ET C'EST TOUT. <br>
-        Choisissez avec soin...
-    </p>
-    <p>
-        Bienvenue sur FreeNote ! <br> Un réseau social où les messages privés n'existent pas. <br>
-        Vous pouvez lancer une discussion sur un sujet (La mort de Jacques Chirac, Pourquoi il ne faut pas boire de Munster, L'utilité d'un BDE, Greta Thunberg, etc.).
-        Puis, vous, cher.ère.s membres, pouvez participer à chaque discussion. <br>
-        Cependant, nous ne sommes pas ici pour créer un basique forum. Chacun de vous ne peut écrire que deux mots dans un message sur toute une discussion ET C'EST TOUT. <br>
-        Choisissez avec soin...
-    </p>
-    <p>
-        Bienvenue sur FreeNote ! <br> Un réseau social où les messages privés n'existent pas. <br>
-        Vous pouvez lancer une discussion sur un sujet (La mort de Jacques Chirac, Pourquoi il ne faut pas boire de Munster, L'utilité d'un BDE, Greta Thunberg, etc.).
-        Puis, vous, cher.ère.s membres, pouvez participer à chaque discussion. <br>
-        Cependant, nous ne sommes pas ici pour créer un basique forum. Chacun de vous ne peut écrire que deux mots dans un message sur toute une discussion ET C'EST TOUT. <br>
-        Choisissez avec soin...
-    </p>
-    <p>
-        Bienvenue sur FreeNote ! <br> Un réseau social où les messages privés n'existent pas. <br>
-        Vous pouvez lancer une discussion sur un sujet (La mort de Jacques Chirac, Pourquoi il ne faut pas boire de Munster, L'utilité d'un BDE, Greta Thunberg, etc.).
-        Puis, vous, cher.ère.s membres, pouvez participer à chaque discussion. <br>
-        Cependant, nous ne sommes pas ici pour créer un basique forum. Chacun de vous ne peut écrire que deux mots dans un message sur toute une discussion ET C'EST TOUT. <br>
-        Choisissez avec soin...
-    </p>
-
 </section>
 <?php
-$discussion1 = array('nick' => 'Paul', 'contenu' => 'Wow');
+$discussion1 = array('nick' => 'Paul', 'contenu' => 'Cookies 🍪');
 $discussion2 = array('nick' => 'Adem', 'contenu' => 'Materialize vie');
 $discussion3 = array('nick' => 'Guillaume', 'contenu' => 'Le PHP');
 $discussion4 = array('nick' => 'Vincent', 'contenu' => 'La BD');
