d818d8616fba3481c3b92ab692a6a968b5ff59e8
guillaume piccina <49280172+guillaume-piccina@users.noreply.github.com>
2019-10-21 17:30:48 +0200

    modif

--- Models/UsersMod.php
+++ Models/UsersMod.php
@@ -1,34 +1,68 @@
 <?php
-    require 'LinkBD.php';
 
-    function createUser($nick, $email, $pwd) {
+require 'Modele.php';
 
+class UsersMod
+{
+
+    public function __construct($c_mail, $c_pwd, $c_nick)
+    {
         $pdo = ConnectBD();
-        $pdo->query("INSERT INTO Utilisateurs(Nom, Mail, MotDePasse, SuperUtilisateur) VALUES ($nick,$email,md5($pwd), '0')");
-
+        $pdo->query("INSERT INTO Utilisateurs(Nom, Mail, MotDePasse, SuperUtilisateur) VALUES ($c_nick,$c_mail,md5($c_pwd), '0')");
     }
 
-    function getPwdBD($nick) {
-
-        $pdo = ConnectBD();
-        $pwd = $pdo->query("SELECT MotDePasse FROM Utilisateurs WHERE Nom = $nick");
-        return $pwd;
-
+    public function getMail()
+    {
+        return $this->mail;
     }
 
-    function setPwdBd($nick, $newPwd) {
-
+    public function setMail($mail)
+    {
         $pdo = ConnectBD();
-        $pdo->query("UPDATE Utilisateurs SET MotDePasse = md5($newPwd) WHERE Nom = $nick");
-
+        $pdo->query("UPDATE Utilisateurs SET Mail = $mail) WHERE Nom = $this->nick");
     }
 
-    function forgetPwd($nick) {
+    public function getPwd()
+    {
+        return $this->pwd;
+    }
 
+    public function setPwd($newPwd)
+    {
+        $pdo = ConnectBD();
+        $pdo->query("UPDATE Utilisateurs SET MotDePasse = md5($newPwd) WHERE Nom = $this->nick");
+    }
+
+    public function getNick()
+    {
+        return $this->nick;
+    }
+
+    public function getAdmin()
+    {
+        return $this->admin;
+    }
+
+    public function setAdmin($admin)
+    {
+        $pdo = ConnectBD();
+        $pdo->query("UPDATE Utilisateurs SET SuperUtilisateur = $admin WHERE Nom = $this->nick");
+    }
+
+    function forgetPwd()
+    {
         $newPwd = uniqid(); //mot de passe aléatoire
-        setPwdBd($nick, $newPwd);
+        $pdo = ConnectBD();
+        $pdo->query("UPDATE Utilisateurs SET MotDePasse = md5($newPwd) WHERE Nom = $this->nick");
         return $newPwd;
     }
+
+}
+
+
+
+
+
 
 
 
