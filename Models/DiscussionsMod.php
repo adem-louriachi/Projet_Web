2b7f6f707b7f2458169ce3ed51247146676413fc
guillaume piccina <49280172+guillaume-piccina@users.noreply.github.com>
2019-10-21 17:32:00 +0200

    modif

--- Models/DiscussionsMod.php
+++ Models/DiscussionsMod.php
@@ -1,8 +1,45 @@
 <?php
-    function createDiscussion($name,$author) {
 
+class DiscussionsMod
+{
+    private $name;
+    private $owner;
+    private $status;
+
+    public function __construct($c_name, $c_owner)
+    {
+        $this->name = $c_name;
+        $this->owner = $c_owner;
         $pdo = ConnectBD();
-        $pdo->query("INSERT INTO Discussion (NomDiscussion, Createur, EstOuvert) VALUES ($name, $author, '1')");
+        $pdo->query("INSERT INTO Discussion (NomDiscussion, Createur, EstOuvert) VALUES ($c_name, $c_owner, '1')");
+    }
+
+    public function GetName()
+    {
+        return $this->name;
+    }
+
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    public function GetStatus()
+    {
+        return $this->status;
+    }
+
+
+    public function GetOwner()
+    {
+        return $this->owner;
+    }
+
+    function createDiscussion($name, $author)
+    {
+
+
 
     }
 
+}
