1cabf18bd4913bbdb2ebe23bcf0f9164cdd0394f
vincent ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-10-23 10:11:17 +0200

    message err et ideee en commentaire

--- ControllerVincentTestBD.php
+++ ControllerVincentTestBD.php
@@ -9,7 +9,7 @@ require 'Models/MessagesMod.php';
         $textMsg = $_POST['Message'];
 
         $message = new MessagesMod($idDis,$textMsg);
-        echo $message;
+        echo $message;  // objet class MessagesMod could not converted to string  Faire fonction Show()
     }
     else{
         echo 'erreur';
