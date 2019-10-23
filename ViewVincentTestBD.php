2dd25988b78fb83ae1168e09d5f888012255b492
vincent ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-10-23 09:29:50 +0200

    Update ViewVincentTestBD.php

--- ViewVincentTestBD.php
+++ ViewVincentTestBD.php
@@ -3,9 +3,9 @@
 require 'Models/MessagesMod.php';
 
 echo '<div class="container black">
-        <form id="register" method="post" action="../Controllers/Register.php">
+        <form id="register" method="post" action="ControllerVincentTestBD.php">
             <label name="Message">Message</label>
             <input name="Message" type="text" required>
-            <button type="submit" value="Envoyer">Envoyer<i>send</i></button>
+            <button type="submit" value="Envoyer">Envoyer</button>
         </form>
     </div>';
