d7e98262acea1209b9ea2f3e978bfe8e0a1f5d47
vincent ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-10-24 13:29:29 +0200

    correction debile

--- ControllerVincentTestBD.php
+++ ControllerVincentTestBD.php
@@ -12,7 +12,7 @@
         $message = new MessagesMod($idDis, $textMsg, $author);
         echo $message->getTextMsg();  // objet class MessagesMod could not converted to string  Faire fonction Show()
     }elseif (isset($_POST['idMsg']) && isset($_POST['MessageU']) && isset($_POST['authorU']) && isset($_POST['etatU'])){
-        $idMsg   = $_POST['idDis'];
+        $idMsg   = $_POST['idMsg'];
         $textMsg = $_POST['Message'];
         $author  = $_POST['author'];
         $state   = $_POST['etatU'];
