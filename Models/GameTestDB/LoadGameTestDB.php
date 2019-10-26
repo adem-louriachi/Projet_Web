0e1b979e463c8be80cc7fdcc5c4ea0ea5e6d0ede
Vincent Ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-10-26 22:39:40 +0200

    require Model.php

--- Models/GameTestDB/LoadGameTestDB.php
+++ Models/GameTestDB/LoadGameTestDB.php
@@ -1,13 +1,14 @@
 <?php
+    require '../Model.php';
 
     class GameTest extends Model {
-        function loadGameTest() {
+        public function loadGameTest() {
             $pdo = Model::connectBD();
             //$sql = 'DELETE FROM Auteur; DELETE FROM Message; DELETE FROM Discussion; DELETE FROM Utilisateurs;';
             $sql = 'DELETE FROM Utilisateurs;';
             Model::executeQuery($pdo, $sql);
 
-            $utilisateurDataFile = 'UtilisateurDB.json';
+            $utilisateurDataFile = 'GameTestDB/UtilisateurDB.json';
             $utilisateurData = file_get_contents($utilisateurDataFile);
             $utilisateurArray = json_decode($utilisateurData, true);
             foreach ($utilisateurArray as $row) {
