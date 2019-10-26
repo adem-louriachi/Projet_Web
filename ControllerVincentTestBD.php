13967ca81c15c289bda130c261c85732a964b977
Vincent Ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-10-26 22:23:58 +0200

    Insert depuis fichier json

---
+++ ControllerVincentTestBD.php
@@ -0,0 +1,35 @@
+<?php
+
+    require 'Models/MessagesMod.php';
+    require 'Models/GameTestDB/LoadGameTestDB.php';
+
+    // Gestion Auteur pas verifier
+
+    if (isset($_POST['idDis']) && isset($_POST['Message']) && isset($_POST['author'])) {
+        $idDis   = $_POST['idDis'];
+        $textMsg = $_POST['Message'];
+        $author  = $_POST['author'];
+
+        $message = new MessagesMod($idDis, $textMsg, $author);
+        $message->InsertMsg();
+        echo $message->getTextMsg();  // objet class MessagesMod could not converted to string  Faire fonction Show()
+        
+    }elseif (isset($_POST['idMsg']) && isset($_POST['MessageU']) && isset($_POST['authorU']) && isset($_POST['etatU'])){
+        $idMsg   = $_POST['idMsg'];
+        $textMsg = $_POST['MessageU'];
+        $author  = $_POST['authorU'];
+        $state   = $_POST['etatU'];
+        MessagesMod::updateMsg($idMsg, $author, $textMsg, $state);
+
+    } elseif (isset($_POST['idMsgC'])){
+        $idMsg   = $_POST['idMsgC'];
+        MessagesMod::closeMsg($idMsg);
+
+    } elseif (isset($_POST['idMsgS'])){
+        $idMsg   = $_POST['idMsgS'];
+        MessagesMod::deleteMsg($idMsg);
+    } elseif (isset($_POST['GameTestDB'])) {
+        loadGameTest();
+    } else{
+        echo 'erreur';
+    }
---
+++ Models/GameTestDB/LoadGameTestDB.php
@@ -0,0 +1,17 @@
+<?php
+
+    function loadGameTest(){
+        $pdo = Model::connectBD();
+        //$sql = 'DELETE FROM Auteur; DELETE FROM Message; DELETE FROM Discussion; DELETE FROM Utilisateurs;';
+        $sql = 'DELETE FROM Utilisateurs;';
+        Model::executeQuery($pdo,$sql);
+
+        $utilisateurDataFile = 'UtilisateurDB.json';
+        $utilisateurData = file_get_contents($utilisateurDataFile);
+        $utilisateurArray = json_decode($utilisateurData, true);
+        foreach ($utilisateurArray as $row){
+            $sql = 'INSERT INTO Utilisateurs(Nom, Mail, MotDePasse, SuperUtilisateur, IdUtilisateur, DateInscription) 
+                    VALUES (\''.$row['Nom'].'\', \''.$row['Mail'].'\', \''.$row['MotDePasse'].'\', \''.$row['SuperUtilisateur'].'\', \''.$row['IdUtilisateur'].'\', \''.$row['DateInscription'].'\')';
+            Model::executeQuery($pdo,$sql);
+        }
+    }
