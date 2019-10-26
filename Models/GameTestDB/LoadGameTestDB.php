4abc543ce10f68eafe83df23fc18b952b98aec55
Vincent Ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-10-26 22:45:38 +0200

    try catch au lieu de executeQuery()

--- Models/GameTestDB/LoadGameTestDB.php
+++ Models/GameTestDB/LoadGameTestDB.php
@@ -1,20 +1,35 @@
 <?php
-//    require '../Model.php';
 
     class GameTest extends Model {
         public function loadGameTest() {
             $pdo = Model::connectBD();
             //$sql = 'DELETE FROM Auteur; DELETE FROM Message; DELETE FROM Discussion; DELETE FROM Utilisateurs;';
             $sql = 'DELETE FROM Utilisateurs;';
-            Model::executeQuery($pdo, $sql);
+            try {
+                $resultat = $pdo->prepare($sql); // requête préparée
+                $resultat->execute();
+                echo $sql . '<br/>';
+            } catch (PDOException $e) {
+                // Affichage de l'erreur
+                echo 'Erreur : ', $e->getMessage(), PHP_EOL;
+                echo 'Requete en faute : ', $sql, PHP_EOL;
+            }
 
-            $utilisateurDataFile = 'GameTestDB/UtilisateurDB.json';
+            $utilisateurDataFile = 'UtilisateurDB.json';
             $utilisateurData = file_get_contents($utilisateurDataFile);
             $utilisateurArray = json_decode($utilisateurData, true);
             foreach ($utilisateurArray as $row) {
                 $sql = 'INSERT INTO Utilisateurs(Nom, Mail, MotDePasse, SuperUtilisateur, IdUtilisateur, DateInscription) 
                         VALUES (\'' . $row['Nom'] . '\', \'' . $row['Mail'] . '\', \'' . $row['MotDePasse'] . '\', \'' . $row['SuperUtilisateur'] . '\', \'' . $row['IdUtilisateur'] . '\', \'' . $row['DateInscription'] . '\')';
-                Model::executeQuery($pdo, $sql);
+                try {
+                    $resultat = $pdo->prepare($sql); // requête préparée
+                    $resultat->execute();
+                    echo $sql . '<br/>';
+                } catch (PDOException $e) {
+                    // Affichage de l'erreur
+                    echo 'Erreur : ', $e->getMessage(), PHP_EOL;
+                    echo 'Requete en faute : ', $sql, PHP_EOL;
+                }
             }
         }
     }
