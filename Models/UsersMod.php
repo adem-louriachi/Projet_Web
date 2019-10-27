bebd96517f4b6c7161480168f32f2e2caef17762
Vincent Ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-10-27 17:41:48 +0100

    analyse pb requete

--- Models/UsersMod.php
+++ Models/UsersMod.php
@@ -25,6 +25,7 @@ class UsersMod extends Model
             $pdo = Model::connectBD();
             $sql = 'INSERT INTO Utilisateurs(Nom, Mail, MotDePasse, SuperUtilisateur) 
                     VALUES (\''.$this->nick.'\',\''.$this->mail.'\',\''.password_hash($this->pwd,PASSWORD_BCRYPT).'\', 0';
+            echo $sql;
             Model::executeQuery($pdo,$sql);
 
             $sql = 'SELECT * FROM Utilisateurs WHERE Nom = \''.$this->nick.'\'';
