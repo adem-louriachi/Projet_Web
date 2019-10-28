8cf582651abe35d71d5046961007a5ae97993110
guillaume-piccina <guillaume.piccina@etu.univ-amu.fr>
2019-10-28 16:21:23 +0100

    modif

--- Views/UsersView.php
+++ Views/UsersView.php
@@ -3,16 +3,16 @@ session_start();
 ob_start();
 $style = 'Views/UsersView.css';
 ?>
-<p>Nom d'utilisateur : <? user.getNick(); ?></p>
-<p>Email : <? user.getMail(); ?></p>
-<p>Date d'inscription : <? user.getDate(); ?>
-<p>Super aministrateur : <? if (user.getAdmin == 0){ echo "non"} else { echo "oui"}; ?>
-<form id="newMail" method="post" action="">
+<p>Nom d'utilisateur : <? $user.getNick(); ?></p>
+<p>Email : <? $user.getMail(); ?></p>
+<p>Date d'inscription : <? $user.getDate(); ?>
+<p>Super aministrateur : <? if ($user.getAdmin() == 0){ echo "non"} else { echo "oui"}; ?>
+<form id="newMail" method="post" action="../Controllers/User.php">
     <label name="email">Changer d'adresse e-mail</label>
         <input name="email" type="email">
     <input id="submit" type="submit" value="Modifier l'adresse mail">
 </form>
-<form id="newPwd" method="post" action="">
+<form id="newPwd" method="post" action="../Controllers/User.php">
     <label name="pwd">Changer de mot de passe</label>
         <input name="pwd" type="password">
     <input id="submit" type="submit" value="Modifier le mot de passe">
