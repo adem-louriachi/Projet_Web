5bd0d93a7b76beda883e8e5778556290b5dfcbd6
guillaume-piccina <guillaume.piccina@etu.univ-amu.fr>
2019-10-28 22:56:02 +0100

    changement provisoire pour test

--- Views/UsersView.php
+++ Views/UsersView.php
@@ -1,22 +1,15 @@
-<?php
-$style = 'Views/UsersView.css';
-ob_start();
-?>
-	<p>Nom d'utilisateur : <? $user->getNick(); ?></p>
-	<p>Email : <? $user->getMail(); ?></p>
-	<p>Date d'inscription : <? $user->getDate(); ?>
-	<p>Super aministrateur : <? if ($user->getAdmin() == 0){ echo "non";} else { echo "oui";}; ?>
-	<form id="newMail" method="post" action="../Controllers/User.php">
-	    <label name="email">Changer d'adresse e-mail</label>
-	        <input name="email" type="email">
-	    <input id="submit" type="submit" value="Modifier l'adresse mail">
-	</form>
-	<form id="newPwd" method="post" action="../Controllers/User.php">
-	    <label name="pwd">Changer de mot de passe</label>
-	        <input name="pwd" type="password">
-	    <input id="submit" type="submit" value="Modifier le mot de passe">
-	</form>
-<?php
-    $content = ob_get_clean();
-    require 'TemplateView.php';
-?>
+
+<p>Nom d'utilisateur : Michel<!--Utiliser la methode getNick--></p>
+<p>Email : Michel@michel<!--Utiliser la methode getMail--></p>
+<p>Date d'inscription : 18 février 2019<!--Utiliser la methode getDate--></p>
+<p>Super administrateur : Non<!--Utiliser la methode getAdmin--></p>
+<form id="newMail" method="post" action="../Controllers/User.php">
+    <label name="email">Changer d'adresse e-mail</label>
+        <input name="email" type="email">
+    <input id="submit" type="submit" value="Modifier l'adresse mail">
+</form>
+<form id="newPwd" method="post" action="../Controllers/User.php">
+    <label name="pwd">Changer de mot de passe</label>
+        <input name="pwd" type="password">
+    <input id="submit" type="submit" value="Modifier le mot de passe">
+</form>
