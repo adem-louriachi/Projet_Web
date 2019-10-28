0ad43711028ceff65a38e2527fa73c4f36971c6b
Vincent Ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-10-28 09:10:38 +0100

    change ini_set

--- Models/MessagesMod.php
+++ Models/MessagesMod.php
@@ -79,7 +79,7 @@
 /*            $sql = 'INSERT INTO Message (IdDisDuMsg, EstOuvert) VALUES ('.$this->idDis.', 1)';
             echo $sql . '<br/>';
             Model::executeQuery($pdo,$sql);
-*/            echo ini_get('memory_limit').'<br/>';
+*/            echo ini_set('memory_limit',512).'<br/>';
 
             $sqlRecupIdMessage = 'SELECT IdMessage FROM Message ORDER BY IdMessage DESC';
             echo $sqlRecupIdMessage. '<br/>';
