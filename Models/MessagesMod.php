e0a204595a4b5b7063c263059e1e368f7f608b19
Vincent Ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-10-28 08:28:18 +0100

    bug INSERT INTO Message

--- Models/MessagesMod.php
+++ Models/MessagesMod.php
@@ -77,6 +77,7 @@
         public function insertMsg(){
             $pdo = Model::connectBD();
             $sql = 'INSERT INTO Message (IdDisDuMsg, EstOuvert) VALUES (\''.$this->idDis.'\', 1)';
+            echo $sql . '<br/>';
             Model::executeQuery($pdo,$sql);
 
             $sqlRecupIdMessage = 'SELECT IdMessage FROM Message ORDER BY IdMessage DESC';
