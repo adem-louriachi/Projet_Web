a0f5f3a12b0aef182c1835326fa912e820f48b79
guillaume-piccina <guillaume.piccina@etu.univ-amu.fr>
2019-10-28 15:41:27 +0100

    modif

--- Views/UsersView.php
+++ Views/UsersView.php
@@ -2,12 +2,22 @@
 session_start();
 ob_start();
 $style = 'Views/UsersView.css';
-$nick = $_SESSION['nick']; // User.getNick();
-$email = $_SESSION['email']; // User.getMail();
 ?>
-<p>Nom d'utilisateur : <?= $nick?></p>
-<p>Email : <?= $email?></p>
-<a href="">Modifier vos informations</a>
+<p>Nom d'utilisateur : <? user.getNick(); ?></p>
+<p>Email : <? user.getMail(); ?></p>
+<p>Date d'inscription : <? user.getDate(); ?>
+<p>Super aministrateur : <? if (user.getAdmin == 0){ echo "non"} else { echo "oui"}; ?>
+<form id="newMail" method="post" action="">
+    <label name="email">Changer d'adresse e-mail</label>
+        <input name="email" type="email">
+    <input id="submit" type="submit" value="Modifier l'adresse mail">
+</form>
+<form id="newPwd" method="post" action="">
+    <label name="pwd">Changer de mot de passe</label>
+        <input name="pwd" type="password">
+    <input id="submit" type="submit" value="Modifier le mot de passe">
+</form>
+
 <?php
     $content = ob_get_clean();
     require 'TemplateView.php';
