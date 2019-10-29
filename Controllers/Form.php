585298965de77667475e05be3512b3e71a166efd
paul-chabas15 <37959800+paul-chabas15@users.noreply.github.com>
2019-10-29 15:23:32 +0100

    Sur un malentendu ça peut marcher

--- Controllers/Form.php
+++ Controllers/Form.php
@@ -5,30 +5,11 @@ class Form
         require 'AuthenticationCheck.php';
         $style = 'Views/HomeView.css';
         ob_start();
-        require 'Views/RegisterView.php';
+
         $nick = filter_input(INPUT_POST, 'nick');
         $email = filter_input(INPUT_POST, 'email');
         $error = '<p style="color:red;">'.filter_input(INPUT_POST, 'error').'</p>';
-        ?>
-        <div class="container black">
-            <form id="register" method="post" action="/?ctrl=User&action=register">
-                <label name="nick">Pseudo</label>
-                <input name="nick" type="text" placeholder="Wankil"
-                       value="<?= $nick?>" autocomplete="nickname" required autofocus>
-                <label name="email">Adresse e-mail</label>
-                <input name="email" type="email" placeholder="coucou@wankil.fr"
-                       value="<?= $email ?>" autocomplete="email" required>
-                <label
-                    name="pwd"><?= $error ?>
-                    Mot de passe</label>
-                <input name="pwd" type="password" autocomplete="new-password" required>
-                <label name="pwdconf">Confirmation du mot de passe</label>
-                <input name="pwdconf" type="password" autocomplete="new-password" required>
-                <button class="submit btn waves-effect waves-light" type="submit" value="Inscription">Soumettre<i
-                        class="material-icons right">send</i></button>
-            </form>
-        </div>
-        <?php
+        require 'Views/RegisterView.php';
         $content = ob_get_clean();
         require 'Views/TemplateView.php';
     }
--- Views/RegisterView.php
+++ Views/RegisterView.php
@@ -1,3 +1,18 @@
-<?php
-
-?>
+<div class="container black">
+    <form id="register" method="post" action="/?ctrl=User&action=register">
+        <label name="nick">Pseudo</label>
+        <input name="nick" type="text" placeholder="Wankil"
+               value="<?= $nick?>" autocomplete="nickname" required autofocus>
+        <label name="email">Adresse e-mail</label>
+        <input name="email" type="email" placeholder="coucou@wankil.fr"
+               value="<?= $email ?>" autocomplete="email" required>
+        <label
+            name="pwd"><?= $error ?>
+            Mot de passe</label>
+        <input name="pwd" type="password" autocomplete="new-password" required>
+        <label name="pwdconf">Confirmation du mot de passe</label>
+        <input name="pwdconf" type="password" autocomplete="new-password" required>
+        <button class="submit btn waves-effect waves-light" type="submit" value="Inscription">Soumettre<i
+                class="material-icons right">send</i></button>
+    </form>
+</div>
