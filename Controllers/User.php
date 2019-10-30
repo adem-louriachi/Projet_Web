d895476e993bfe512bcf44fe8f348dab4aab4814
guillaume-piccina <guillaume.piccina@etu.univ-amu.fr>
2019-10-30 21:19:04 +0100

    ajout de la methode signin

--- Controllers/User.php
+++ Controllers/User.php
@@ -46,6 +46,7 @@ class User
     {
         if (UsersMod::testLoginPwd($_POST['login'], $_POST['pwd'])) //vérifie l'existance du login et pwd dans la base
         {
+            UsersMod::signin($_POST['login']);
             session_start();
             $_SESSION['user'] = $_POST['login'];
             header('location: Home.php');
