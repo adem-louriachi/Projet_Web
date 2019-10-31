5412d550d088677d3de41751cdf58eb93812b3bf
Vincent Ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-10-31 10:14:44 +0100

    avec le require c'est mieux

--- ControllerVincentTestBD.php
+++ ControllerVincentTestBD.php
@@ -1,7 +1,8 @@
 <?php
 
-      require 'Models/MessagesMod.php';
+//    require 'Models/MessagesMod.php';
 //    require 'Models/UsersMod.php';
+    require 'Models/DiscussionsMod.php';
     require 'Models/GameTestDB/LoadGameTestDB.php';
 
     if (isset($_POST['idDis']) && isset($_POST['Message']) && isset($_POST['author'])) {
