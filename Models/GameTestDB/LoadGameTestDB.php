7e4a9b402a1ba1d4534cbd79b244c3b281a8a7e5
Vincent Ferrer <45425884+vincent-ferrer@users.noreply.github.com>
2019-10-31 18:12:52 +0100

    Suppression des echo inutile servant au test

--- Models/GameTestDB/LoadGameTestDB.php
+++ Models/GameTestDB/LoadGameTestDB.php
@@ -19,7 +19,6 @@
             foreach ($utilisateurArray as $row) {
                 $sql = 'INSERT INTO Utilisateurs(Nom, Mail, MotDePasse, SuperUtilisateur, IdUtilisateur, DateInscription) 
                         VALUES (\'' . addcslashes($row['Nom'],'\'') . '\', \'' . $row['Mail'] . '\', \'' . $row['MotDePasse'] . '\', \'' . $row['SuperUtilisateur'] . '\', \'' . $row['IdUtilisateur'] . '\', \'' . $row['DateInscription'] . '\')';
-                echo $sql. '<br/>';
                 Model::executeQuery($pdo,$sql);
             }
 
@@ -29,7 +28,6 @@
             foreach ($discussionArray as $row) {
                 $sql = 'INSERT INTO Discussion (IdDiscussion, EstOuvert, Createur, NomDiscussion) 
                         VALUES (\'' . $row['IdDiscussion'] . '\', \'' . $row['EstOuvert'] . '\', \'' . $row['Createur'] . '\', \'' . addcslashes($row['NomDiscussion'],'\'') . '\')';
-                echo $sql. '<br/>';
                 Model::executeQuery($pdo,$sql);
             }
 
@@ -39,7 +37,6 @@
             foreach ($msgArray as $row) {
                 $sql = 'INSERT INTO Message (IdMessage, IdDisDuMsg, Date, EstOuvert) 
                         VALUES (\'' . $row['IdMessage'] . '\', \'' . $row['IdDisDuMsg'] . '\', \'' . $row['Date'] . '\', \'' . $row['EstOuvert'] . '\')';
-                echo $sql. '<br/>';
                 Model::executeQuery($pdo,$sql);
             }
 
@@ -49,7 +46,6 @@
             foreach ($msgArray as $row) {
                 $sql = 'INSERT INTO SectionMessage (IdSection, IdMessage, Auteur, TextSection, Date) 
                         VALUES (\'' . $row['IdSection'] . '\', \'' . $row['IdMessage'] . '\', \'' . $row['Auteur'] . '\', \'' . addcslashes($row['TextSection'],'\'') . '\', \'' . $row['Date'] . '\')';
-                echo $sql. '<br/>';
                 Model::executeQuery($pdo,$sql);
             }
         }
