27a2783faa45b5171662ccf09475a73839537be2
guillaume-piccina <guillaume.piccina@etu.univ-amu.fr>
2019-11-02 17:02:45 +0100

    ajout require

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -16,13 +16,13 @@
 <div class="discussion collection container">
 <?php
 
-require 'Models/DiscussionsMod.php';
+
 $nbDiscussion = DiscussionsMod::getNbDiscussion();
 $i = 0;
 while ($i < $nbDiscussion) { ?>
     <article>
         <a href="#!" class="discussion collection-item active">
-            <h3 class="center-align"><? DiscussionsMod::showNameDis(i); ?></h3>
+            <h3 class="center-align"><? DiscussionsMod::showNameDis($i); ?></h3>
             <p class="left-align"></p>
         </a>
     </article>
