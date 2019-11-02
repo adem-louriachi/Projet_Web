cf92ea7a7a7c1cc081a03da598a4f1ec4c2c1235
paul-chabas15 <37959800+paul-chabas15@users.noreply.github.com>
2019-11-02 23:03:05 +0100

    Messages de test

--- Models/UsersMod.php
+++ Models/UsersMod.php
@@ -123,6 +123,7 @@ class UsersMod extends Model {
             }
             $sql = 'UPDATE Utilisateurs SET MotDePasse = \'' . password_hash($newPwd, PASSWORD_BCRYPT) . '\' WHERE Mail = \'' . $email . '\'';
             Model::executeQuery($pdo, $sql);
+            echo 'Fin du forgetPwd';
             return true; // ( succès du changement de mot de passe )
         } catch (Exception $e) {
             $_SESSION['error'] = $e->getMessage();
