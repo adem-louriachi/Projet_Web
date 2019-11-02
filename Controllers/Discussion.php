8a5550fe3c07149ef8da61fb57daa83d7cc43cf0
guillaume-piccina <guillaume.piccina@etu.univ-amu.fr>
2019-11-02 21:50:37 +0100

    modif boucle

--- Controllers/Discussion.php
+++ Controllers/Discussion.php
@@ -12,20 +12,18 @@ class Discussion{
         $id = $_GET['id'];
         $allIdMsg = MessagesMod::getAllMessage($id);
         $maxIdMsg = 0;
-        while ($idMsg = $allIdMsg->fetch()) {//obtenir le plus grand idMessage
-            if ($idMsg > $maxIdMsg) { $maxIdMsg = $idMsg; }
-        }?>
-        <form id="register" method="post" action="<? MessagesMod::insertSectionMsg($maxIdMsg, $_SESSION['user'], $_POST['message']) ?> >" >
-            <input name="message" type="text" placeholder="Message...">
-            <button class="submit btn waves-effect waves-light" type="submit">Envoyer<i
-                        class="material-icons right">send</i></button>
-        </form>
-        <?php
-        while ( $idMsg = $allIdMsg->fetch()) { ?>
+        while ( $idMsg = $allIdMsg->fetch()) {
+            if ($idMsg > $maxIdMsg) { $maxIdMsg = $idMsg; } //obtenir le plus grand idMessage (le dernier) ?>
             <article>
                 <p><? echo MessagesMod::getTxt($idMsg['IdMessage']); ?></p>
             </article>
-        <?php }
+        <?php } ?>
+            <form id="register" method="post" action="<? MessagesMod::insertSectionMsg($maxIdMsg, $_SESSION['user'], $_POST['message']) ?> >" >
+                <input name="message" type="text" placeholder="Message...">
+                <button class="submit btn waves-effect waves-light" type="submit">Envoyer<i
+                        class="material-icons right">send</i></button>
+            </form>
+        <?php
         $content = ob_get_clean();
         require 'Views/TemplateView.php';
 
