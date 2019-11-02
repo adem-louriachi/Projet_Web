baf418c7981a64597b897ac50c4f2b10d7586d8b
guillaume-piccina <guillaume.piccina@etu.univ-amu.fr>
2019-11-02 19:37:21 +0100

    affichage msg

--- Controllers/Discussion.php
+++ Controllers/Discussion.php
@@ -1,29 +1,8 @@
 <?php
 
 class Discussion{
-    private $name;
-    private $owner;
-    private $status;
 
-    public function __construct($c_name, $c_owner){
-        $this->name = $c_name;
-        $this->owner = $c_owner;
-        $this->statut = False;
-    }
-
-    public function GetStatus(){
-        return $this->status;
-    }
-
-    public function GetName(){
-        return $this->name;
-    }
-
-    public function GetOwner(){
-        return $this->owner;
-    }
-
-    public function show(){ // affiche la discussion qu'on lui donne
+    public static function show(){ // affiche la discussion qu'on lui donne
         require 'AccountMenu.php';
         $style = 'Views/HomeView.css';
         ob_start();
