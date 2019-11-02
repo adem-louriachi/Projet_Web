a5dcad51920a34db3dd215ba6080a4121719a058
paul-chabas15 <37959800+paul-chabas15@users.noreply.github.com>
2019-11-02 12:51:16 +0100

    Correction toujours

--- Controllers/User.php
+++ Controllers/User.php
@@ -42,13 +42,15 @@ class User
     {
         $isID = (preg_match('/^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/', $_POST['nick'])) ? false : true;
         $userData = UsersMod::signin($_POST['nick'], $_POST['pwd'], $isID);
-        $_SESSION = [
-            'nick' => $userData['Nom'],
-            'email' => $userData['Mail'],
-            'date' => $userData['DateInscription'],
-            'admin' => $userData['SuperUtilisateur']
-        ];
-        header('location: /');
+        if (isset($userData)) {
+            $_SESSION = [
+                'nick' => $userData['Nom'],
+                'email' => $userData['Mail'],
+                'date' => $userData['DateInscription'],
+                'admin' => $userData['SuperUtilisateur']
+            ];
+            header('location: /');
+        }
     }
 
     public static function signout()
