c34196626b7a5b3eb93a0f9ea4279b2d05bcc78c
Vincent Ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-11-03 14:22:16 +0100

    Bouton GameTest correction

--- Controllers/AccountMenu.php
+++ Controllers/AccountMenu.php
@@ -1,6 +1,7 @@
 <?php
     if ($_GET['ctrl'] != 'User') require 'User.php';
     ob_start();
+    echo '<li><button name = "LoadGameTest" class="submit btn waves-effect waves-light" type="submit">Charger GameTest</button></li>';
     if (User::isConnected()){
         echo '<li><a href="/?ctrl=User&action=view" class="waves-effect waves-light btn">Mon compte</a></li><li><a href="/?ctrl=User&action=signout">Se déconnecter</a></li>';
     }
@@ -10,12 +11,8 @@
             <li><a href="/?ctrl=Form&action=register" class="waves-effect waves-light btn">S\'inscrire</a></li><li><a href="/?ctrl=Form&action=forget">Mot de passe oublié</a></li>';
     }
 
-    echo '<li><button name = "LoadGameTest" class="submit btn waves-effect waves-light" type="submit">Charger GameTest<i
-                            class="material-icons right">load</i></button></li>';
-
     if (isset($_POST['LoadGameTest'])) {
         require 'Models/GameTestDB/LoadGameTestDB.php';
         GameTest::loadGameTest();
     }
-
     $auth = ob_get_clean();
