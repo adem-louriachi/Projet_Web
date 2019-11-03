73a5dc3c30c086dcdbdeb89f32640e868c3cfa83
Vincent Ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-11-03 10:15:59 +0100

    avec des accolades c'est mieux

--- Controllers/Discussion.php
+++ Controllers/Discussion.php
@@ -12,12 +12,12 @@ class Discussion{
         $idDis = $_GET['id'];
         $allIdMsg = MessagesMod::getAllMessage($idDis);
         $maxIdMsg = 0;
-        foreach($allIdMsg as $idMsg['IdMessage'] => $id)
+        foreach($allIdMsg as $idMsg['IdMessage'] => $id) {
             if ($id > $maxIdMsg) { $maxIdMsg = $id; } //obtenir le plus grand idMessage (le dernier) ?>
         <article>
-
             <p><? echo $id; ?></p>
         </article>
+        <?php   } ?>
 
         <?php /*
         while ( $idMsg = $allIdMsg->fetch()) {
@@ -26,6 +26,8 @@ class Discussion{
                 <p><? echo MessagesMod::getTxt($idMsg['IdMessage']); ?></p>
             </article>
         <?php } */ ?>
+
+        
         <?php
             if(User::isConnected())
             {
