c49f867c735d6a8266ea7974b5afac12d9e5d798
Vincent Ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-11-03 17:45:43 +0100

    test

--- Controllers/Discussion.php
+++ Controllers/Discussion.php
@@ -21,7 +21,7 @@ class Discussion
             if ($id['IdMessage'] > $maxIdMsg) { $maxIdMsg = $id['IdMessage']; }
             ?>
             <article>
-                <p><? self::butDeleteMsg($id['IdMessage']); echo MessagesMod::getAuthorsForMsg($id['IdMessage'])?> : <? echo MessagesMod::getTxt($id['IdMessage']); ?></p>
+                <p><? echo $id['IdMessage']; self::butDeleteMsg($id['IdMessage']); echo MessagesMod::getAuthorsForMsg($id['IdMessage'])?> : <? echo MessagesMod::getTxt($id['IdMessage']); ?></p>
             </article>
         <?php }
 
