90b7345560fcac2b86420b10add04eac67c7a493
Adem LOURIACHI <louriachi.adem@gmail.com>
2019-11-03 21:01:44 +0100

    Mise en forme Mon Compte

--- Views/UsersView.php
+++ Views/UsersView.php
@@ -4,26 +4,27 @@
 <p>Email : <?=$_SESSION['email']?></p>
 <p>Date d'inscription : <?=$_SESSION['date']?></p>
 <p>Super administrateur : <?=$_SESSION['isAdmin']?></p>
-        <div class="col right-align s6">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc-cK1ui78ki3ur0b4h_OvNdBIhqLGMaRrpV1Dbu-dyTNmyqdMaA&amp;s" class="circle responsive-img">
-        </div>
     </div>
 </div>
-<form id="newMail" method="post" action="/?ctrl=User&action=changeMail">
-    <?= $_SESSION['error']['changeEmail'] ?>
-    <label>Changer d'adresse e-mail
-        <input name="email" type="email" placeholder="Nouvelle adresse e-mail">
-    </label>
-    <button class="submit btn waves-effect waves-light" type="submit" value="Modifier l'adresse email">Modifier l'adresse email<i class="material-icons right">send</i></button>
-</form>
-<form id="newPwd" method="post" action="/?ctrl=User&action=changePwd">
-    <?= $_SESSION['error']['changePwd'] ?>
-    <label>Changer de mot de passe
-        <input name="pwd" type="password" placeholder="Nouveau mot de passe">
-        <input name="pwdconf" type="password" placeholder="Confirmation du nouveau mot de passe">
-    </label>
-    <button class="submit btn waves-effect waves-light" type="submit" value="Modifier le mot de passe">Modifier le mot de passe<i class="material-icons right">send</i></button>
-</form>
+<div class="row">
+    <div class="col s6">
+    <form id="newMail" method="post" action="/?ctrl=User&action=changeMail">
+        <?= $_SESSION['error']['changeEmail'] ?>
+        <label>Changer d'adresse e-mail
+            <input name="email" type="email" placeholder="Nouvelle adresse e-mail">
+        </label>
+        <button class="submit btn waves-effect waves-light" type="submit" value="Modifier l'adresse email">Modifier l'adresse email<i class="material-icons right">send</i></button>
+    </form>
+    <form id="newPwd" method="post" action="/?ctrl=User&action=changePwd">
+        <?= $_SESSION['error']['changePwd'] ?>
+        <label>Changer de mot de passe
+            <input name="pwd" type="password" placeholder="Nouveau mot de passe">
+            <input name="pwdconf" type="password" placeholder="Confirmation du nouveau mot de passe">
+        </label>
+        <button class="submit btn waves-effect waves-light" type="submit" value="Modifier le mot de passe">Modifier le mot de passe<i class="material-icons right">send</i></button>
+    </form>
+    </div>
+</div>
 <form method="post" action="/?ctrl=User&action=delete">
     <?= $_SESSION['error']['delete'] ?>
     <label>Afin de confirmer votre volonté de supprimer votre compte, veuillez écrire "supprimer VOTREPSEUDO" en remplaçant VOTREPSEUDO par le pseudo de votre compte
