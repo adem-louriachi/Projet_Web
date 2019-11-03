dfeb04c5a7814dcbaba6f3220813db0de2f1d204
Vincent Ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-11-03 18:08:01 +0100

    avec un formulaire la methode post marchera mieux pour charger le gameTest

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -16,7 +16,11 @@
 </div>
 <div class="row">
     <div class="col s6 right-align">
-   <? echo '<li><button name = "LoadGameTest" class="waves-effect waves-light btn" type="submit">Charger GameTest</button></li>';
+   <? echo '<form id="GameTestForm" method="post" action="">
+                <button name = "LoadGameTest" class="waves-effect waves-light btn" type="submit">Charger GameTest</button>
+            </form>';
+
+
    if (isset($_POST['LoadGameTest'])) {
        require 'Models/GameTestDB/LoadGameTestDB.php';
        GameTest::loadGameTest();
