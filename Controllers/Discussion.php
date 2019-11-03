a2cd42ee29b2781f802e2b00645802635b5597b2
Vincent Ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-11-03 10:39:25 +0100

    ajout acolade if

--- Controllers/Discussion.php
+++ Controllers/Discussion.php
@@ -1,9 +1,11 @@
 <?php
 
 
-class Discussion{
+class Discussion
+{
 
-    public static function show(){ // affiche la discussion qu'on lui donne
+    public static function show()
+    { // affiche la discussion qu'on lui donne
         require 'AccountMenu.php';
         require 'Models/MessagesMod.php';
         $style = 'Views/HomeView.css';
@@ -12,14 +14,15 @@ class Discussion{
         $idDis = $_GET['id'];
         $allIdMsg = MessagesMod::getAllMessage($idDis);
         $maxIdMsg = 0;
-        foreach($allIdMsg as $idMsg['IdMessage'] => $id) {
-            if ($id['IdMessage'] > $maxIdMsg) { $maxIdMsg = $id['IdMessage']; ?>
-            <article>
-                <p><? echo MessagesMod::getTxt($id['IdMessage']); ?></p>
-            </article>
-        <?php   } ?>
+        foreach ($allIdMsg as $idMsg['IdMessage'] => $id) {
+            if ($id['IdMessage'] > $maxIdMsg) {
+                $maxIdMsg = $id['IdMessage']; }?>
+                <article>
+                    <p><? echo MessagesMod::getTxt($id['IdMessage']); ?></p>
+                </article>
+        <?php } ?>
 
-        <?php /*
+            <?php /*
         while ( $idMsg = $allIdMsg->fetch()) {
             if ($idMsg['IdMessage'] > $maxIdMsg) { $maxIdMsg = $idMsg['IdMessage']; } //obtenir le plus grand idMessage (le dernier) ?>
             <article>
@@ -29,27 +32,26 @@ class Discussion{
 
 
         <?php
-            if(User::isConnected())
-            {
-        ?>
-            <form id="register" method="post" action="" >
+        if (User::isConnected()) {
+            ?>
+            <form id="register" method="post" action="">
                 <input name="message" type="text" placeholder="Message...">
                 <button class="submit btn waves-effect waves-light" type="submit">Envoyer<i
-                        class="material-icons right">send</i></button>
+                            class="material-icons right">send</i></button>
             </form>
-        <?php
-                if (MessagesMod::getState($maxIdMsg) == 1) { //vérifie si le msg est ouvert
+            <?php
+            if (MessagesMod::getState($maxIdMsg) == 1) { //vérifie si le msg est ouvert
 
-                    if (isset($_POST['message']) && !empty($_POST['message'])) {
-                        MessagesMod::insertSectionMsg($maxIdMsg, $_SESSION['nick'], $_POST['message']);
-                        if (MessagesMod::countSection($maxIdMsg) == 3 ) { //nouveau message quand le dernier est plein
-                            MessagesMod::closeMsg($maxIdMsg);
-                            MessagesMod::insertMsg($idDis);
-                        }
-                        header('refresh: 1');
+                if (isset($_POST['message']) && !empty($_POST['message'])) {
+                    MessagesMod::insertSectionMsg($maxIdMsg, $_SESSION['nick'], $_POST['message']);
+                    if (MessagesMod::countSection($maxIdMsg) == 3) { //nouveau message quand le dernier est plein
+                        MessagesMod::closeMsg($maxIdMsg);
+                        MessagesMod::insertMsg($idDis);
                     }
+                    header('refresh: 1');
                 }
             }
+        }
         $content = ob_get_clean();
         require 'Views/TemplateView.php';
 
